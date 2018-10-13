71cf6c8e927a17cd4ecfbd35096ed43058b90035
akshaycj <akshaycj999@gmail.com>
2018-10-13 22:41:19 +0530

    Route for login to home

--- src/Components/DigitLogin/index.js
+++ src/Components/DigitLogin/index.js
@@ -1,35 +1,43 @@
 import React, { Component } from "react";
-import './index.css'
-import logo from '../../Res/logo.svg'
-import { Input } from 'antd'
+import "./index.css";
+import logo from "../../Res/logo.svg";
+import { Input } from "antd";
+import { Link } from "react-router-dom";
 
 export default class extends Component {
-
   render() {
-
     return (
-      <div className="" >
-        <div className="digit-main" >
-          <div className="digit-top" >
+      <div className="">
+        <div className="digit-main">
+          <div className="digit-top">
             <img src={logo} width="75px" height="75px" />
-            <div className="digit-top-text"><b>Cowostash</b></div>
+            <div className="digit-top-text">
+              <b>Cowostash</b>
+            </div>
           </div>
 
-          <div className="digit-mid" >
+          <div className="digit-mid">
             Enter your 8 digit connection key and connect your device
           </div>
 
           <div>
-            <div className="digit-bottom-layout" >
-              <Input placeholder="8 digit connection key" style={{ borderRadius: 300, marginLeft: 4, height: 40 }} />
-              <div className="digit-bottom-button">Connect</div>
+            <div className="digit-bottom-layout">
+              <Input
+                placeholder="8 digit connection key"
+                style={{ borderRadius: 300, marginLeft: 4, height: 40 }}
+              />
+              <div className="digit-bottom-button">
+                <Link
+                  to="/home"
+                  style={{ textDecoration: "none", color: "white" }}
+                >
+                  Connect
+                </Link>
+              </div>
             </div>
           </div>
-          
         </div>
-        <div className="digit-bottom-text" >
-            help@cowostash.com
-          </div>
+        <div className="digit-bottom-text">help@cowostash.com</div>
       </div>
     );
   }
