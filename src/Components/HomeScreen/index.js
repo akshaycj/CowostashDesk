912a7cc88d41fded2343331baa79ac16a030ac57
akshaycj <akshaycj999@gmail.com>
2018-10-13 22:49:56 +0530

    Added Visitor details form

--- src/Components/HomeScreen/index.js
+++ src/Components/HomeScreen/index.js
@@ -1,34 +1,29 @@
 import React, { Component } from "react";
-import './index.css'
-import logo from '../../Res/logo.svg'
-import qr from '../../Res/qr.png'
+import "./index.css";
+import logo from "../../Res/logo.svg";
+import qr from "../../Res/qr.png";
 
 export default class extends Component {
-
   render() {
-
     return (
-      <div >
+      <div>
         <div className="homeS-main">
-          <img src={logo} width="100" height="100"  />
-         <div className="homeS-middle" >
-         <b style={{fontSize:62}} >
-            Hello
-          </b>
-          <div style={{fontSize:36,marginTop:15}} >
-            Select purpose editable Text
+          <img src={logo} width="100" height="100" />
+          <div className="homeS-middle">
+            <b style={{ fontSize: 62 }}>Hello</b>
+            <div style={{ fontSize: 36, marginTop: 15 }}>
+              Select your purpose of visit
+            </div>
+            <div style={{ display: "flex" }}>
+              <div className="homeS-inner">Visitor</div>
+              <div className="homeS-inner">Checkin</div>
+            </div>
+            <div style={{ fontSize: 26, marginTop: 35, cursor: "pointer" }}>
+              Got a QR? Scan Now <img src={qr} width="35" height="35" />
+            </div>
           </div>
-          <div className="homeS-inner" >
-            Checkin
-          </div>
-          <div style={{fontSize:26, marginTop:15, cursor:'pointer'}} >
-            Got a QR? Scan Now <img src={qr} width="35" height="35" />
-          </div>
-         </div>
         </div>
-        <div className="digit-bottom-text" >
-          help@cowostash.com
-          </div>
+        <div className="digit-bottom-text">help@cowostash.com</div>
       </div>
     );
   }
---
+++ src/Components/VisitorDetailsForm/index.js
@@ -0,0 +1,89 @@
+import React, { Component } from "react";
+import './index.css'
+import logo from '../../Res/logo.svg'
+import { Select } from 'antd'
+
+
+
+const Option = Select.Option;
+
+
+const purposes = [
+  {
+    data: 'Delivery Boy',
+    value: 'db',
+  },
+  {
+    data: 'Employee',
+    value: 'emp',
+  },
+  {
+    data: 'Contract Employee',
+    value: 'ce',
+  },
+  {
+    data: 'Vendor',
+    value: 'ven',
+  },
+  {
+    data: 'Maintanence Worker',
+    value: 'mw',
+  },
+  {
+    data: 'Guest, Family & Friends',
+    value: 'fam',
+  },
+  {
+    data: 'Interview',
+    value: 'int',
+  },
+  {
+    data: 'Event',
+    value: 'evt',
+  },
+  {
+    data: 'Other',
+    value: 'otr',
+  }
+
+]
+
+export default class extends Component {
+
+
+  render() {
+
+
+
+    return (
+      <div className="visitor-layout" >
+        <div className="visitor-main" >
+          <img src={logo} width={60} height={60} />
+          <div className="visitor-card" >
+            <div className="visitor-card-text" >
+              Select purpose of your visit
+            </div>
+            <div>
+              <Select
+                className="visitor-card-select"
+                placeholder="Select purpose"
+              >
+                
+                {purposes.map((data) => <Option value={data.value} >{data.data} </Option>)}
+
+
+              </Select>
+            </div>
+            <div className="visitor-bottom-button" >
+              Next >
+            </div>
+          </div>
+        </div>
+
+        <div className="visitor-bottom-text" >
+          help@cowostash.com
+        </div>
+      </div>
+    );
+  }
+}
