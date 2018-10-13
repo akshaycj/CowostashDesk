1b3edf0abf77f260fa6065b6cd978854c05abe0b
akshaycj <akshaycj999@gmail.com>
2018-10-14 00:15:46 +0530

    Added Checkboxes to the form

--- src/Components/VisitorForm/index.js
+++ src/Components/VisitorForm/index.js
@@ -13,7 +13,7 @@ export default class extends Component {
         <div className="form">
           <Input
             size="large"
-            style={{ marginTop: 10 }}
+            style={{ marginTop: 20 }}
             addonBefore={
               <div
                 style={{
@@ -30,7 +30,7 @@ export default class extends Component {
           />
           <Input
             size="large"
-            style={{ marginTop: 10 }}
+            style={{ marginTop: 20 }}
             addonBefore={
               <div
                 style={{
@@ -47,7 +47,7 @@ export default class extends Component {
           />
           <Input
             size="large"
-            style={{ marginTop: 10 }}
+            style={{ marginTop: 20 }}
             addonBefore={
               <div
                 style={{
@@ -64,7 +64,7 @@ export default class extends Component {
           />
           <Input
             size="large"
-            style={{ marginTop: 10 }}
+            style={{ marginTop: 20 }}
             addonBefore={
               <div
                 style={{
@@ -79,11 +79,32 @@ export default class extends Component {
               </div>
             }
           />
-          <Select style={{ marginTop: 10, width: "100%" }}>
+          <Select
+            placeholder="Select your age group"
+            style={{ marginTop: 20, width: "100%" }}
+          >
             <Option value="Below 18">Below 18</Option>
             <Option value="18-25">18-25</Option>
             <Option value="25-35">25-35</Option>
+            <Option value="Above 35">Above 35</Option>
           </Select>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              marginTop: 20,
+              marginLeft: 10,
+              textAlign: "left",
+              width: "100%"
+            }}
+          >
+            Marital Status:
+            <div style={{ marginTop: 20, width: "100%", display: "flex" }}>
+              <Checkbox>Married</Checkbox>
+              <Checkbox>Unmarried</Checkbox>
+              <Checkbox>Prefer not to say</Checkbox>
+            </div>
+          </div>
         </div>
       </div>
     );
